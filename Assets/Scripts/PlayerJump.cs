886349f1db9ac15e26d7c9b10c05b2ccaac95bc8
amirandagoeller6623 <amirandagoeller6623@student.pps.net>
2025-06-11 11:33:13 -0700

    last

--- Assets/Scripts/PlayerJump.cs
+++ Assets/Scripts/PlayerJump.cs
@@ -8,16 +8,18 @@ public class PlayerJump : MonoBehaviour
     // Start is called before the first frame update
     private Rigidbody rigidBody;
     private float jumpForce = 4f;
+    private GroundController _groundController;
     // Update is called once per frame
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();  
+        _groundController = GetComponent<GroundController>();
     }
     void Update()
     {
         if (Input.GetButtonDown("Jump"))
         {
-            Jump();
+                Jump();
         }
     }
     private void Jump()
